d2912646019da8fdd125e5fe42083ef00ba1e57b
Keira Jarvis <82812348+KeiraJCoder@users.noreply.github.com>
2023-06-02 18:16:31 +0100

    Add files via upload

---
+++ merch.js
@@ -0,0 +1,10 @@
+
+  $(document).ready(function(){
+    $("#carousel").owlCarousel({
+      items: 1,
+      loop: true,
+      nav: true,
+      dots: true
+      // You can customize the carousel options as per your requirements
+    });
+  });
